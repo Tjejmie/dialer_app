44141afccadc09bee4a105f5b00dd5c67609ccd2
JamieSvanberg <jasv2000@student.miun.se>
2022-11-07 14:26:31 +0100

    New activity for dial created

--- app/src/main/java/se/miun/jasv2000/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/jasv2000/dt031g/dialer/MainActivity.java
@@ -1,14 +1,41 @@
 package se.miun.jasv2000.dt031g.dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+import se.miun.jasv2000.dt031g.dialer.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        openActivityDial();
+
+
+
     }
+
+    public void openActivityDial() {
+        //Button button_dial = (Button) findViewById(R.id.button_dial);
+        binding.buttonDial.setOnClickListener(v -> {
+            Intent intent = new Intent("android.intent.category.DIAL");
+            startActivity(intent);
+        }
+        );
+    }
+
+
+
 }
